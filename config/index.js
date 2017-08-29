76456701e565c5991d1ed2f4f7539f949d2097d2
unknown <905107931@qq.com>
2017-08-29 16:22:50 +0800

    1.0

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     // port: 9000,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: '',
+    assetsPublicPath: './',
     staticPath:'./', //生产环境 staticPath:''
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
